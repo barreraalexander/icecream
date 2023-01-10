0e5359aa8ed390697f7222ba851dca13a94cc25d
barreraalexander <barreraalexander93@gmail.com>
2023-01-09 19:36:49 -0500

    update to the js

--- website/static/js/test.js
+++ website/static/js/test.js
@@ -1,8 +1,14 @@
-alert('Hi Jane, glad you can make it')
-alert("why don't you like eggs?")
+let check_run = window.localStorage.getItem('ran_once')
+if (! check_run){
+    alert('Hi Jane, glad you can make it')
+    alert("why don't you like eggs?")
+    alert("check the test.js file and you'll see why this annoying little prompt only occurs once :) ")
+    window.localStorage.setItem('ran_once', 'true')
+}
+
+
 
 let good_food = document.querySelector('#good_food')
-
 if (good_food){
     good_food.addEventListener('mouseover', run_me, false)
 }
