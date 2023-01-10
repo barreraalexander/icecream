06d04d4af2e4da4ac800e07d1b9759c64730b221
barreraalexander <barreraalexander93@gmail.com>
2023-01-09 19:29:13 -0500

    update to the code

--- website/static/js/test.js
+++ website/static/js/test.js
@@ -1,3 +1,12 @@
 alert('Hi Jane, glad you can make it')
 alert("why don't you like eggs?")
 
+let good_food = document.querySelector('#good_food')
+
+if (good_food){
+    good_food.addEventListener('mouseover', run_me, false)
+}
+
+function run_me(){
+    alert("please don't touch the food mam")
+}
